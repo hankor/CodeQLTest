1624c095ff1e22f78d69b5b10fbd33a970606586
Hanno Kortekamp <hanno.kortekamp@bertelsmann.de>
2023-10-05 16:12:01 +0200

    Create 01_03_07Controller.cs

---
+++ Controllers/01_03_07Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeQLTest.Controllers
+{
+    [Route("01_03_07")]
+    public class BAM01_03_07Controller : Controller
+    {
+        [Route("")]
+        public async Task<IActionResult> Index([FromQuery] string path)
+        {
+            if (new FileInfo(path).Exists)
+            {
+                //Wait for longer operation to complete
+                await new DatabaseContext().Users.ToListAsync();
+                //Might be deleted already
+                System.IO.File.Delete(path);
+            }
+            return Ok();
+        }
+    }
+}
